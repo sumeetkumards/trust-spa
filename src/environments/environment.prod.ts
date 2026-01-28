013f63c13a7398855dc3b2b8401dc975763655fc
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2026-01-28 15:47:25 +0530

    angular-update-gallery to include videos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  youtube_api_key: 'AIzaSyD8GBxYRTItIwdIZFbxbV5lMepx_kCB9Oc',
+  youtube_playlist_id: 'PLmUE7KcUXS5GGgB8b0EQlTaOOi-uLRilE&si=oS_olQWYeioCrvd2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  youtube_api_key: 'AIzaSyD8GBxYRTItIwdIZFbxbV5lMepx_kCB9Oc',
+  youtube_playlist_id: 'PLmUE7KcUXS5GGgB8b0EQlTaOOi-uLRilE&si=oS_olQWYeioCrvd2'
 };
 
 /*
